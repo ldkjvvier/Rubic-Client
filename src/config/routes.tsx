016e7c8394770ab8c17b69350ade10ed2fa333d9
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-06-19 00:16:46 -0400

    refactor: Update routes configuration and component imports

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,43 +1,63 @@
 import { Home as UserHome } from '@/modules/core';
 import { Home as AdminHome } from '@/modules/admin';
-import { Login, Register, ProtectedRoute } from '@/modules/auth';
+import { NotFound } from '@/components/NotFound';
+import { Login, ProtectedRoute } from '@/modules/auth';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useAuth } from '@/modules/auth/hooks/useAuth';
+
+const PublicRoutes = () => {
+  return (
+    <Routes>
+      {/* RUTAS PUBLICAS */}
+      <Route path="*" element={<Login />} />
+    </Routes>
+  );
+};
+const UserRoutes = () => {
+  return (
+    <Routes>
+      {/* RUTAS DEL USUARIO */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute role="user" redirectTo="/">
+            <UserHome />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+const AdminRoutes = () => {
+  return (
+    <Routes>
+      {/* RUTAS DEL ADMINISTRADOR */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute role="admin" redirectTo="/admin">
+            <AdminHome />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
 
 export const AppRoutes = () => {
+  const { isAuthenticated, user } = useAuth();
+
   return (
     <BrowserRouter>
-      <Routes>
-        {/* RUTAS PUBLICAS */}
-        <Route path="/" element={<Login />} />
-        <Route
-          path="*"
-          element={
-            <div>
-              <h1>404</h1>
-              <p>Página no encontrada</p>
-            </div>
-          }
-        />
-        <Route path="/register" element={<Register />} />
-        {/* RUTAS DEL ADMINISTRADOR */}
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute role="admin" redirectTo="/admin">
-              <AdminHome />
-            </ProtectedRoute>
-          }
-        />
-        {/* RUTAS DEL USUARIO */}
-        <Route
-          path="/user"
-          element={
-            <ProtectedRoute role="user" redirectTo="/">
-              <UserHome />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      {isAuthenticated && user.role === 'admin' ? (
+        <AdminRoutes />
+      ) : isAuthenticated && user.role === 'user' ? (
+        <UserRoutes />
+      ) : (
+        <PublicRoutes />
+      )}
     </BrowserRouter>
   );
 };
