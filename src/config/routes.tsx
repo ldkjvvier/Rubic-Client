64e0e52b13d8290ea13f3ef7b0acefa997825eaf
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-06-18 22:02:15 -0400

    chore: Update routes configuration and component imports

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,42 +1,43 @@
-import { Home } from '@/modules/core'
-import { Login, Register, ProtectedRoute } from '@/modules/auth'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home as UserHome } from '@/modules/core';
+import { Home as AdminHome } from '@/modules/admin';
+import { Login, Register, ProtectedRoute } from '@/modules/auth';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const AppRoutes = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				{/* RUTAS PUBLICAS */}
-				<Route path="/" element={<Login />} />
-				<Route
-					path="*"
-					element={
-						<div>
-							<h1>404</h1>
-							<p>Página no encontrada</p>
-						</div>
-					}
-				/>
-				<Route path="/register" element={<Register />} />
-				{/* RUTAS DEL ADMINISTRADOR */}
-				<Route
-					path="/admin"
-					element={
-						<ProtectedRoute role="admin" redirectTo="/admin">
-							<div>ROL DE ADMINISTRADOR</div>
-						</ProtectedRoute>
-					}
-				/>
-				{/* RUTAS DEL USUARIO */}
-				<Route
-					path="/user"
-					element={
-						<ProtectedRoute role="user" redirectTo="/">
-							<Home />
-						</ProtectedRoute>
-					}
-				/>
-			</Routes>
-		</BrowserRouter>
-	)
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* RUTAS PUBLICAS */}
+        <Route path="/" element={<Login />} />
+        <Route
+          path="*"
+          element={
+            <div>
+              <h1>404</h1>
+              <p>Página no encontrada</p>
+            </div>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+        {/* RUTAS DEL ADMINISTRADOR */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute role="admin" redirectTo="/admin">
+              <AdminHome />
+            </ProtectedRoute>
+          }
+        />
+        {/* RUTAS DEL USUARIO */}
+        <Route
+          path="/user"
+          element={
+            <ProtectedRoute role="user" redirectTo="/">
+              <UserHome />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
