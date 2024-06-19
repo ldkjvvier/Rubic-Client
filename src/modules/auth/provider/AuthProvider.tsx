f30aa29a4be34ef4999a192c4424d49d160cb0d2
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-06-18 23:00:36 -0400

    chore: Refactor AuthProvider component and add signin functionality

--- src/modules/auth/provider/AuthProvider.tsx
+++ src/modules/auth/provider/AuthProvider.tsx
@@ -1,93 +1,122 @@
-import { useContext, createContext, useState, useEffect } from 'react'
-import { AuthResponse, User } from '../types/user'
-import { verifyTokenService } from '../services/verifyTokenService'
-interface AuthProviderProps {
-	children: React.ReactNode
-}
+import { createContext, useState, useEffect } from 'react';
+import { AuthResponse, LoginData, User } from '../types/user';
+import { verifyTokenService } from '../services/verifyTokenService';
+import { Navigate } from 'react-router';
+import { loginService } from '../api/auth';
 
-const AuthContext = createContext({
-	isAuthenticated: false,
-	saveUser: (userData: AuthResponse) => {},
-	user: {} as User,
-	isLoading: false,
-	logout: () => {},
-})
+interface AuthContextType {
+  isAuthenticated: boolean;
+  user: User;
+  error: string;
+  saveUser: (userData: AuthResponse) => void;
+  isLoading: boolean;
+  logout: () => void;
+  signin: (credentials: LoginData) => void;
+}
 
 const defaultUser = {
-	id: '',
-	name: '',
-	lastname: '',
-	email: '',
-	role: '',
-} as User
-export const AuthProvider = ({ children }: AuthProviderProps) => {
-	const [isAuthenticated, setIsAuthenticated] = useState(false)
-	const [user, setUser] = useState<User>(defaultUser)
-	const [isLoading, setIsLoading] = useState(true)
+  id: '',
+  name: '',
+  lastname: '',
+  email: '',
+  role: ''
+} as User;
 
-	useEffect(() => {
-		const checkAuth = async () => {
-			setIsLoading(true)
-			try {
-				const data = await verifyTokenService()
-				if (data.statusCode !== 200) {
-					setIsAuthenticated(false)
-					setIsLoading(false)
-					return
-				}
-				console.log(user)
-				saveSessionInfo(data.body.user)
-				setIsAuthenticated(true)
-				setIsLoading(false)
-			} catch (error) {
-				setIsAuthenticated(false)
-				setIsLoading(false)
-			}
-		}
-		checkAuth()
-	}, [])
+const AuthContext = createContext({} as AuthContextType);
+export const AuthProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState<User>(defaultUser);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+  useEffect(() => {
+    const checkAuth = async () => {
+      setIsLoading(true);
+      try {
+        const data = await verifyTokenService();
+        if (data.statusCode !== 200) {
+          return unauthorized();
+        }
+        console.log(data);
+        saveSessionInfo(data.body.user);
+        setIsAuthenticated(true);
+        setIsLoading(false);
+      } catch (error) {
+        return unauthorized();
+      }
+    };
+    checkAuth();
+  }, []);
 
-	const saveSessionInfo = (user: User) => {
-		console.log({ user })
-		setIsAuthenticated(true)
-		setUser(user)
-	}
+  const unauthorized = () => {
+    setIsAuthenticated(false);
+    setUser(defaultUser);
+    setIsLoading(false);
+    return <Navigate to="/" />;
+  };
+  const saveSessionInfo = (user: User) => {
+    console.log({ user });
+    setIsAuthenticated(true);
+    setUser(user);
+    return;
+  };
 
-	const saveUser = (userData: AuthResponse) => {
-		saveSessionInfo(userData.body.user)
-	}
+  const saveUser = (userData: AuthResponse) => {
+    saveSessionInfo(userData.body.user);
+  };
 
-	const logout = async () => {
-		try {
-			const res = await fetch(
-				'http://localhost:5000/v1/api/auth/logout',
-				{
-					method: 'POST',
-					credentials: 'include',
-				}
-			)
-			const data = await res.json()
-			if (data.statusCode === 200) {
-				setIsAuthenticated(false)
-				setUser(defaultUser)
-			}
-		} catch (error) {
-			console.log(error)
-		}
-	}
-	return (
-		<AuthContext.Provider
-			value={{
-				isAuthenticated,
-				user,
-				saveUser,
-				isLoading,
-				logout,
-			}}
-		>
-			{children}
-		</AuthContext.Provider>
-	)
-}
+  const logout = async () => {
+    try {
+      const res = await fetch('http://localhost:5000/v1/api/auth/logout', {
+        method: 'POST',
+        credentials: 'include'
+      });
+      const data = await res.json();
+      if (data.statusCode === 200) {
+        unauthorized();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-export const useAuth = () => useContext(AuthContext)
+  const signin = async (credentials: LoginData) => {
+    setIsLoading(true);
+    setError('');
+
+    try {
+      const res = await loginService(credentials);
+      console.log(res);
+      saveUser(res);
+
+      setIsAuthenticated(true);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError('Error en el inicio de sesión');
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        user,
+        error,
+        saveUser,
+        isLoading,
+        logout,
+        signin
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext }; // Asegúrate de exportar AuthContext
+export default AuthProvider;
