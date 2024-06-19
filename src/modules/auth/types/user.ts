9969deab69f8e5267a921c9f98581f4bc0242730
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-06-18 23:01:00 -0400

    refactor: Update User interface in auth module

--- src/modules/auth/types/user.ts
+++ src/modules/auth/types/user.ts
@@ -1,19 +1,20 @@
 export interface AuthResponse {
-	statusCode: number
-	body: {
-		user: User
-	}
+  statusCode: number;
+  body: {
+    user: User;
+    message?: string;
+  };
 }
 
 export interface User {
-	id: string
-	name: string
-	lastname: string
-	email: string
-	role?: string
+  id: string;
+  name: string;
+  lastname: string;
+  email: string;
+  role?: string;
 }
 
 export interface LoginData {
-	email: string
-	password: string
+  email: string;
+  password: string;
 }
