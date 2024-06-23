d0dcddf727286ea77baab4241d75aecdce2501a0
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-06-23 19:29:18 -0400

    refactor: Update tailwind.config.js to extend theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		extend: {
-			backgroundColor: {
-				primary: '#0e0f12',
-				secondary: '#1e1f23',
-				tertiary: '#2e2f33',
-			},
-		},
-	},
-	plugins: [],
-}
-
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      backgroundColor: {
+        primary: '#0e0f12',
+        secondary: '#1e1f23',
+        tertiary: '#2e2f33'
+      },
+      borderColor: {
+        primary: '#0e0f12',
+        secondary: '#1e1f23',
+        tertiary: '#2e2f33'
+      },
+      textColor: {
+        primary: '#ffffff',
+        secondary: '#d1d5db',
+        tertiary: '#9ca3af'
+      }
+    }
+  },
+  plugins: []
+};
