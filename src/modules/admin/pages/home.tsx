7ff1246a11fdd774f722cb74c5a46759b3374d38
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-06-24 19:06:11 -0400

    refactor: Update home page layout and add PDF upload functionality

--- src/modules/admin/pages/home.tsx
+++ src/modules/admin/pages/home.tsx
@@ -1,11 +1,19 @@
 import { Header } from '@/modules/shared';
+import { PdfUrl } from '../components/PdfUrl';
+import { PdfFile } from '../components/PdfFile';
+import { AutoHeightOverlayNoSnap } from '../components/Uploaded';
 export const Home = () => {
   return (
     <>
       <Header />
 
-      <div>
-        <h1>Admin Page</h1>
+      <div className="flex flex-col justify-center align-middle px-4 md:px-32 ">
+        <h1 className="my-16">Sube tu archivo PDF</h1>
+        <div className="grid md:grid-cols-2 w-full pb-3 gap-3">
+          <PdfUrl />
+          <PdfFile />
+        </div>
+        <AutoHeightOverlayNoSnap />
       </div>
     </>
   );
