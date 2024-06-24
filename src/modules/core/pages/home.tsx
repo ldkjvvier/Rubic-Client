fcfbea4de7473c2ba027e027379f462dcd666ce2
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-06-24 19:07:10 -0400

    refactor: Update home page to display PDF list and handle user authentication

--- src/modules/core/pages/home.tsx
+++ src/modules/core/pages/home.tsx
@@ -1,15 +1,26 @@
-import { PdfUrl } from '../components/PdfUrl';
-import { PdfFile } from '../components/PdfFile';
 import { Header } from '@/modules/shared';
+import { PdfList } from '../components/PdfList';
+import { useAuth } from '@/modules/auth/hooks/useAuth';
+import { useEffect } from 'react';
+import { usePdf } from '../hooks/usePdf';
 export const Home = (): JSX.Element => {
+  const { user } = useAuth();
+  const { data, loading, error, fetchData } = usePdf();
+  useEffect(() => {
+    fetchData(user.name, user.lastname);
+  }, [user]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="w-full">
       <Header />
-
-      <PdfUrl />
-      <br />
-      <PdfFile />
-      <br />
+      <div className="container mx-auto p-4">
+        {data && <PdfList data={data} />}
+        {error && <div>Error: {error}</div>}
+      </div>
     </div>
   );
 };
