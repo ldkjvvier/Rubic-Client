edff63a5e64f96cbe41d6789a11304a74556eb3d
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-06-24 19:03:15 -0400

    feat: Add FormLayout component for consistent form styling

---
+++ src/modules/admin/components/FormLayout.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+
+export const FormLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Box
+      className="border border-tertiary rounded-sm px-4 py-6 
+      bg-tertiary bg-opacity-10 text-white text-center "
+    >
+      {children}
+    </Box>
+  );
+};
