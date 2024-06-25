9a02c0e012a646a7900e3f84f9fc0cafdfb4f1bc
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-06-25 02:28:39 -0400

    refactor: Update NotFound component to improve styling and structure

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,8 +1,50 @@
+import { Container, Typography, Button, Stack } from '@mui/material';
+
 export const NotFound = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Página no encontrada</p>
+    <div className="flex items-center  h-full">
+      <Container maxWidth="xs" className="bg-whi te border-quaternary rounded-lg p-12 flex justify-items-center">
+        <Typography
+          variant="h4"
+          component="h1"
+          sx={{
+            color: 'lightgray',
+            fontSize: '9rem',
+            fontWeight: 'bold'
+          }}
+        >
+          404
+        </Typography>
+        <Typography
+          variant="h5"
+          component="h2"
+          sx={{
+            color: 'darkgray'
+          }}
+        >
+          Pagina no encontrada
+        </Typography>
+
+        <Stack spacing={2}>
+          <Typography
+            variant="body1"
+            sx={{
+              color: 'gray'
+            }}
+          >
+            La página que buscas no existe o ha sido movida.
+          </Typography>
+          <Button
+            href="/"
+            variant="text"
+            sx={{
+              color: 'lightSlateGray'
+            }}
+          >
+            Volver al inicio
+          </Button>
+        </Stack>
+      </Container>
     </div>
   );
 };
